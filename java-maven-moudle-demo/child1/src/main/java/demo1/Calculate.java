3557148678e9e40ec66dca8c1063b0fba6e61487
letian <sunlt1699@gmail.com>
2019-04-28 08:04:24 +0800

    add maven module demo

---
+++ java-maven-moudle-demo/child1/src/main/java/demo1/Calculate.java
@@ -0,0 +1,9 @@
+package demo1;
+
+public class Calculate {
+
+    public static int add(int a, int b) {
+        return a+b;
+    }
+
+}
---
+++ java-maven-moudle-demo/child2/src/main/java/demo2/CalculateService.java
@@ -0,0 +1,12 @@
+package demo2;
+
+import demo1.Calculate;
+
+public class CalculateService {
+
+    public static void main(String[] args) {
+        int result = Calculate.add(1, 2);
+        System.out.println(result);
+    }
+
+}
